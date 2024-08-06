8b553b17db8f0235519b2c8a8f8a9b1f2c1bc18d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-08-06 13:39:11 +0100

    add  new vehicle

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const mysql = require('mysql');
 const path = require('path');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 const conn = mysql.createPool({
     host: 'localhost',
@@ -13,14 +17,31 @@ const conn = mysql.createPool({
     database: 'autorout'
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     conn.query('SELECT * FROM vehicule', (err, results) => {
         if (err) {
             return res.status(500).send('Database query error');
         }
-        res.render('index', { vehicles: results });
+        res.render('index', { vehicles: results, Title: 'Vehicles List' });
+    });
+});
+
+app.get('/add', (req, res) => {
+    res.render('create', { Title: 'Add new vehicle' });
+});
+
+app.post('/add', (req, res) => {
+    const { type, immatriculation, louee, user_id } = req.body;
+    const isLouee = louee ? 1 : 0;
+    const immatriculationInt = parseInt(immatriculation, 10);
+
+    const query = 'INSERT INTO vehicule (type, immatriculation, louee, user_id) VALUES (?, ?, ?, ?)';
+    conn.query(query, [type, immatriculationInt, isLouee, user_id], (err, results) => {
+        if (err) {
+            console.error('Database insert error: ', err);
+            return res.status(500).send('Database insert error: ' + err.message);
+        }
+        res.redirect('/');
     });
 });
 
